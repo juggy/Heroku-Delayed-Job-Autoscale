a9f07abb8ae85afb7bd3928df42cb2446b8e51b0
Julien Guimont <julien@porkepic.com>
2011-04-21 09:10:31 -0400

    Catch errors from heroku api and send them to the log and hoptoad (if available)

--- lib/heroku_delayed_job_autoscale/managers/heroku.rb
+++ lib/heroku_delayed_job_autoscale/managers/heroku.rb
@@ -1,25 +1,52 @@
 require 'heroku'
 
+hoptoad_loaded = true
+begin
+  require 'hoptoad_notifier'
+rescue
+  hoptoad_loaded = false
+end
+
 module HerokuDelayedJobAutoscale
   module Manager
     class Heroku
       def initialize(options={})
-        username = options[:username] || ENV['HEROKU_USERNAME']
-        password = options[:password] || ENV['HEROKU_PASSWORD']
-        @app     = options[:app]      || ENV['HEROKU_APP']
-        @client = ::Heroku::Client.new(username, password)
+        begin
+          username = options[:username] || ENV['HEROKU_USERNAME']
+          password = options[:password] || ENV['HEROKU_PASSWORD']
+          @app     = options[:app]      || ENV['HEROKU_APP']
+          @client = ::Heroku::Client.new(username, password)
+        rescue => e
+          notify_hoptoad(e) if hoptoad_loaded
+          Rails.logger.error e
+        end
       end
 
       def qty
-        @client.info(@app)[:workers].to_i
+        begin
+          @client.info(@app)[:workers].to_i
+        rescue => e
+          notify_hoptoad(e) if hoptoad_loaded
+          Rails.logger.error e
+        end
       end
 
       def scale_up
-        @client.set_workers(@app, 1)
+        begin
+          @client.set_workers(@app, 1)
+        rescue => e
+          notify_hoptoad(e) if hoptoad_loaded
+          Rails.logger.error e
+        end
       end
 
       def scale_down
-        @client.set_workers(@app, 0)
+        begin
+          @client.set_workers(@app, 0)
+        rescue => e
+          notify_hoptoad(e) if hoptoad_loaded
+          Rails.logger.error e
+        end
       end
     end
   end
